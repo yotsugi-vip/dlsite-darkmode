bdac3d478b8380fa0a7e9d618d616d189b23ca05
yotsugi <dev01.topaz@gmail.com>
2021-01-12 20:46:30 +0900

    init gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const output = "./public/style/";
+const input = "./src/style/**/*.scss";
+
+/** コンパイル */
+gulp.task('sass', (fn) => {
+    gulp.src(input)
+        .pipe(sass())
+        .pipe(gulp.dest(output));
+
+    console.log('now compile...');
+    fn();
+});
+
+/** 変更監視 */
+gulp.task('watch',(fn)=>{
+    gulp.watch(input, gulp.task('sass'));
+
+    console.log('gulp watch start')
+    fn();
+});
+
+gulp.task('default', gulp.task('sass'));
