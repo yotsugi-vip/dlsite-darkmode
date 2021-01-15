31107aac60794fb7a7873eb8092eb14931a6db1b
yotsugi <dev01.topaz@gmail.com>
2021-01-15 15:25:25 +0900

    scssコンパイル時統合

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const glob = require('gulp-sass-glob');
 const output = "./public/style/";
 const input = "./src/style/**/*.scss";
 
 /** コンパイル */
 gulp.task('sass', (fn) => {
     gulp.src(input)
+        .pipe(glob())
         .pipe(sass())
         .pipe(gulp.dest(output));
 
@@ -14,7 +16,7 @@ gulp.task('sass', (fn) => {
 });
 
 /** 変更監視 */
-gulp.task('watch',(fn)=>{
+gulp.task('watch', (fn) => {
     gulp.watch(input, gulp.task('sass'));
 
     console.log('gulp watch start')
